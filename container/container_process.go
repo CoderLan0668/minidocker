5182d04d7ffcbef008e7b79be534c18e88c730ac
CoderLan0668 <lanjian_0668@163.com>
2024-06-27 03:35:26 +0000

    add feat-pidns

--- container/container_process.go
+++ container/container_process.go
@@ -3,10 +3,14 @@ package container
 import (
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func NewContainerProcess(tty bool, command string) *exec.Cmd {
 	cmd := exec.Command(command)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWPID,
+	}
 	if tty {
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
